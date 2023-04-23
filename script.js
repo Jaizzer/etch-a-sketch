21a1cc199531cffe97ddf3f030ed0919684b43e6
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-04-24 07:13:23 +0800

    Implement loop for creating 16^2 divs

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let gridContainer = document.createElement("div");
+
+for (let i = 0; i < 16**2; i++) {
+    let square = document.createElement("div");
+    square.className = "square";
+    gridContainer.appendChild(square);
+}
