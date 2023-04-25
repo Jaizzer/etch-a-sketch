3c698ffabf92e9ae1d5f3432a7f8f574e11f1c1f
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-04-26 07:17:18 +0800

    Put grid creation expressions inside function to abstract it

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let gridContainer = document.createElement("div");
-gridContainer.className = "grid-container";
+function createGrid() {
 
-for (let i = 0; i < 16**2; i++) {
-    let gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-    gridContainer.appendChild(gridItem);
+    let gridContainer = document.createElement("div");
+    gridContainer.className = "grid-container";
 
-    gridItem.addEventListener("mouseenter", changeColor);
+    for (let i = 0; i < 16 ** 2; i++) {
+        let gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+        gridContainer.appendChild(gridItem);
+
+        gridItem.addEventListener("mouseenter", changeColor);
+    }
+    document.body.appendChild(gridContainer);
 }
-
-document.body.appendChild(gridContainer);
 
 let button = document.createElement("button");
 button.textContent = "Create New Grid";
