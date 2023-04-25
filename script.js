c22c7d5ad312ea378497c82958b361f2a8c226e9
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-04-26 06:37:05 +0800

    Rename 'square divs' to 'grid-item' for clarity

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let gridContainer = document.createElement("div");
 gridContainer.className = "grid-container";
 
 for (let i = 0; i < 16**2; i++) {
-    let square = document.createElement("div");
-    square.className = "square";
-    gridContainer.appendChild(square);
+    let gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridContainer.appendChild(gridItem);
 }
 
 document.body.appendChild(gridContainer);
