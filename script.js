15f4678c927812c8084b0ae452a48631e5e5cddd
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-04-26 06:58:08 +0800

    Add event listener to change div's color when hovered

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ for (let i = 0; i < 16**2; i++) {
     let gridItem = document.createElement("div");
     gridItem.className = "grid-item";
     gridContainer.appendChild(gridItem);
+
+    gridItem.addEventListener("mouseenter", changeColor);
 }
 
 document.body.appendChild(gridContainer);
